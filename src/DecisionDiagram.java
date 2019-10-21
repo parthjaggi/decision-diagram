cc650e1bc8c0a187b46ee88a898f5b2c89594fa6
parthjaggi <parthjaggi28@gmail.com>
2019-10-21 09:27:54 -0400

    reduceOptimized;

--- src/DecisionDiagram.java
+++ src/DecisionDiagram.java
@@ -1,6 +1,5 @@
 import java.lang.reflect.Field;
 import java.util.HashMap;
-import java.util.Map;
 
 import util.IntTriple;
 
@@ -18,6 +17,7 @@ public class DecisionDiagram {
     // RC: reduceCache, AC: applyCache
     HashMap<Integer, Integer> tNodeValueIdC = new HashMap<Integer, Integer>();
     HashMap<IntTriple, Integer> iNodeTripletIdC = new HashMap<IntTriple, Integer>();
+    HashMap<Integer, Integer> reduceC = new HashMap<Integer, Integer>();
 
     DecisionDiagram() {
     };
@@ -172,6 +172,47 @@ public class DecisionDiagram {
             if (tNodeValueIdC.containsKey(ltnode.value)) {
                 node.low = tNodeValueIdC.get(ltnode.value);
             }
+        }
+    }
+
+    public Integer reduceOptimised(Integer nId) {
+        ANode n = idNodeMap.get(nId);
+        if (n instanceof TNode) {
+            Integer vId = tNodeValueIdC.get(((TNode)n).value);
+            if (vId != null) {
+                return vId;
+            } else {
+                tNodeValueIdC.put(((TNode)n).value, nId);
+                return nId;
+            }
+        }
+        if (reduceC.containsKey(nId)) {
+            return reduceC.get(nId);
+        } else {
+            INode in = (INode) n;
+            reduceOptimised(in.high);
+            reduceOptimised(in.low);
+
+            Integer rId = getReducedNode(nId, in.value, in.high, in.low);
+
+            reduceC.put(nId, rId);
+            return rId;
+        }
+    }
+
+    public Integer getReducedNode(Integer nId, String value, Integer highId, Integer lowId) {
+        if (highId == lowId) {
+            return highId;
+        }
+        Integer vId = valueIdMap.get(value);
+        IntTriple triple = new IntTriple(vId, highId, lowId);
+
+        Integer rId = iNodeTripletIdC.get(triple);
+        if (rId != null){
+            return rId;
+        } else {
+            iNodeTripletIdC.put(triple, nId);
+            return nId;
         }
     }
 
@@ -345,7 +386,9 @@ public class DecisionDiagram {
         // dd.addNodeLink(1, NodeType.EXTERNAL.type(), LinkType.LOW.type(), 0);
         // dd.addNodeLink(2, NodeType.EXTERNAL.type(), LinkType.HIGH.type(), 1);
         // dd.addNodeLink(2, NodeType.EXTERNAL.type(), LinkType.LOW.type(), 0);
-        // dd.reduce(dd.root, null);
+        // // dd.reduce(dd.root, null);
+        // Integer id = dd.reduceOptimised(0);
+        // dd.print(dd.idNodeMap.get(id));
 
         /* TESTING ADD OPERATION */
         // initialize 2 dds, and perform add operation on them
@@ -381,74 +424,74 @@ public class DecisionDiagram {
         // | x1 | x3 |
         // actions: 0, 1, 2, 3 (top, right, bottom, left). reward at x4 is 1
         // get utility at x2
-        HashMap<Integer, Integer> actionValueMap = new HashMap<Integer, Integer>();
-        HashMap<Integer, String> actionFStateMap = new HashMap<Integer, String>(); // future state
-        actionFStateMap.put(1, "x4");
-        actionFStateMap.put(2, "x1");
+        // HashMap<Integer, Integer> actionValueMap = new HashMap<Integer, Integer>();
+        // HashMap<Integer, String> actionFStateMap = new HashMap<Integer, String>(); // future state
+        // actionFStateMap.put(1, "x4");
+        // actionFStateMap.put(2, "x1");
 
-        // utility at t=0, V0prime (primed)
-        DecisionDiagram V0prime = new DecisionDiagram("x4'");
-        V0prime.addNodeLink(0, NodeType.EXTERNAL.type(), LinkType.HIGH.type(), 1);
-        V0prime.addNodeLink(0, NodeType.EXTERNAL.type(), LinkType.LOW.type(), 0);
-        // V0prime.print(V0prime.root);
+        // // utility at t=0, V0prime (primed)
+        // DecisionDiagram V0prime = new DecisionDiagram("x4'");
+        // V0prime.addNodeLink(0, NodeType.EXTERNAL.type(), LinkType.HIGH.type(), 1);
+        // V0prime.addNodeLink(0, NodeType.EXTERNAL.type(), LinkType.LOW.type(), 0);
+        // // V0prime.print(V0prime.root);
 
-        Integer bestAction = null;
-        Integer bestActionEval = 0;
+        // Integer bestAction = null;
+        // Integer bestActionEval = 0;
 
-        for (Map.Entry<Integer, String> actionFStateEntry : actionFStateMap.entrySet()) {
-            // create probability DD based on future state.
-            DecisionDiagram Q = new DecisionDiagram(actionFStateEntry.getValue() + "'");
-            Q.addNodeLink(0, NodeType.INTERNAL.type(), LinkType.HIGH.type(), "x2");
-            Q.addNodeLink(0, NodeType.INTERNAL.type(), LinkType.LOW.type(), "x2");
-            Q.addNodeLink(1, NodeType.EXTERNAL.type(), LinkType.HIGH.type(), 1);
-            Q.addNodeLink(1, NodeType.EXTERNAL.type(), LinkType.LOW.type(), 0);
-            Q.addLink(2, 3, LinkType.LOW.type()); // to value 1
-            Q.addLink(2, 4, LinkType.HIGH.type()); // to value 0
-            // Q.print(Q.root);
+        // for (Map.Entry<Integer, String> actionFStateEntry : actionFStateMap.entrySet()) {
+        //     // create probability DD based on future state.
+        //     DecisionDiagram Q = new DecisionDiagram(actionFStateEntry.getValue() + "'");
+        //     Q.addNodeLink(0, NodeType.INTERNAL.type(), LinkType.HIGH.type(), "x2");
+        //     Q.addNodeLink(0, NodeType.INTERNAL.type(), LinkType.LOW.type(), "x2");
+        //     Q.addNodeLink(1, NodeType.EXTERNAL.type(), LinkType.HIGH.type(), 1);
+        //     Q.addNodeLink(1, NodeType.EXTERNAL.type(), LinkType.LOW.type(), 0);
+        //     Q.addLink(2, 3, LinkType.LOW.type()); // to value 1
+        //     Q.addLink(2, 4, LinkType.HIGH.type()); // to value 0
+        //     // Q.print(Q.root);
 
-            // multiply with V0prime
-            DecisionDiagram F = new DecisionDiagram();
-            DecisionDiagram F2 = new DecisionDiagram();
-            F.addRoot(DecisionDiagram.operate(Q.root, V0prime.root, Q, V0prime, F, new Operate.Multiply()));
-            F2.addRoot(DecisionDiagram.operate(Q.root, V0prime.root, Q, V0prime, F2, new Operate.Multiply()));
-            F.reduce(F.root, null);
-            F2.reduce(F2.root, null);
-            // F.print(F.root);
+        //     // multiply with V0prime
+        //     DecisionDiagram F = new DecisionDiagram();
+        //     DecisionDiagram F2 = new DecisionDiagram();
+        //     F.addRoot(DecisionDiagram.operate(Q.root, V0prime.root, Q, V0prime, F, new Operate.Multiply()));
+        //     F2.addRoot(DecisionDiagram.operate(Q.root, V0prime.root, Q, V0prime, F2, new Operate.Multiply()));
+        //     F.reduce(F.root, null);
+        //     F2.reduce(F2.root, null);
+        //     // F.print(F.root);
 
-            // summation over primed variables in Q.
-            HashMap<String, Integer> assignMap = new HashMap<String, Integer>();
-            assignMap.put(actionFStateEntry.getValue() + "'", 0);
-            DecisionDiagram.partialEval(0, null, null, assignMap, F);
-            assignMap.put(actionFStateEntry.getValue() + "'", 1);
-            DecisionDiagram.partialEval(0, null, null, assignMap, F2);
-            DecisionDiagram G = new DecisionDiagram();
-            G.addRoot(DecisionDiagram.operate(F.root, F2.root, F, F2, G, new Operate.Add()));
-            // G.print(G.root);
-            // System.out.println();
+        //     // summation over primed variables in Q.
+        //     HashMap<String, Integer> assignMap = new HashMap<String, Integer>();
+        //     assignMap.put(actionFStateEntry.getValue() + "'", 0);
+        //     DecisionDiagram.partialEval(0, null, null, assignMap, F);
+        //     assignMap.put(actionFStateEntry.getValue() + "'", 1);
+        //     DecisionDiagram.partialEval(0, null, null, assignMap, F2);
+        //     DecisionDiagram G = new DecisionDiagram();
+        //     G.addRoot(DecisionDiagram.operate(F.root, F2.root, F, F2, G, new Operate.Add()));
+        //     // G.print(G.root);
+        //     // System.out.println();
 
-            // evaluate
-            HashMap<String, Integer> evalAssignMap = new HashMap<String, Integer>();
-            evalAssignMap.put("x4'", 0); // todo: unprime all variables in G. eval non-present vars to 0.
-            evalAssignMap.put("x2", 1);
-            Integer eval = DecisionDiagram.eval(G.nodeIdMap.get(G.root), evalAssignMap, G);
-            actionValueMap.put(actionFStateEntry.getKey(), eval);
+        //     // evaluate
+        //     HashMap<String, Integer> evalAssignMap = new HashMap<String, Integer>();
+        //     evalAssignMap.put("x4'", 0); // todo: unprime all variables in G. eval non-present vars to 0.
+        //     evalAssignMap.put("x2", 1);
+        //     Integer eval = DecisionDiagram.eval(G.nodeIdMap.get(G.root), evalAssignMap, G);
+        //     actionValueMap.put(actionFStateEntry.getKey(), eval);
 
-            if (eval > bestActionEval) {
-                bestActionEval = eval;
-                bestAction = actionFStateEntry.getKey();
-            }
-        }
+        //     if (eval > bestActionEval) {
+        //         bestActionEval = eval;
+        //         bestAction = actionFStateEntry.getKey();
+        //     }
+        // }
 
-        // future reward
-        DecisionDiagram FR = new DecisionDiagram(bestActionEval);
+        // // future reward
+        // DecisionDiagram FR = new DecisionDiagram(bestActionEval);
 
-        DecisionDiagram V0 = new DecisionDiagram("x4");
-        V0.addNodeLink(0, NodeType.EXTERNAL.type(), LinkType.HIGH.type(), 1);
-        V0.addNodeLink(0, NodeType.EXTERNAL.type(), LinkType.LOW.type(), 0);
+        // DecisionDiagram V0 = new DecisionDiagram("x4");
+        // V0.addNodeLink(0, NodeType.EXTERNAL.type(), LinkType.HIGH.type(), 1);
+        // V0.addNodeLink(0, NodeType.EXTERNAL.type(), LinkType.LOW.type(), 0);
 
-        DecisionDiagram V1 = new DecisionDiagram();
-        V1.addRoot(DecisionDiagram.operate(V0.root, FR.root, V0, FR, V1, new Operate.Add()));
-        V1.print(V1.root);
+        // DecisionDiagram V1 = new DecisionDiagram();
+        // V1.addRoot(DecisionDiagram.operate(V0.root, FR.root, V0, FR, V1, new Operate.Add()));
+        // V1.print(V1.root);
 
         /* TESTING SPUDD - HARDCODED */
         // initialize 2 dds, multiply them, summation over variable
